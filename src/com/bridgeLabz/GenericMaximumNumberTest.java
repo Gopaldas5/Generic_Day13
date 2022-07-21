4be5148e359f8071d9c7a51460dda0f2636229b1
GopalDas <gopalkrdas1997@gmail.com>
2022-07-22 01:43:20 +0530

    Refactor: create Generic Class to take in 3 variables of Generic Type

--- src/com/bridgeLabz/GenericMaximumNumberTest.java
+++ src/com/bridgeLabz/GenericMaximumNumberTest.java
@@ -5,53 +5,55 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenericMaximumNumberTest {
+    GenericMaximumNumber<Integer> intObj = new GenericMaximumNumber();
+    GenericMaximumNumber<Float> floatObj = new GenericMaximumNumber();
+    GenericMaximumNumber<String> stringObj = new GenericMaximumNumber();
+
     @Test
     void firstMaxIntNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        int max = GenericMaximumNumber.maximum(3, 6, 1);
-        assertEquals(6,max);
+        int maxInt = intObj.maximum(3, 6, 1);
+        assertEquals(6,maxInt);
     }
     @Test
     void secondMaxIntNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        int max = GenericMaximumNumber.maximum(7, 6, 10);
-        assertEquals(7,max);
+
+        int maxInt = intObj.maximum(7, 6, 10);
+        assertEquals(7,maxInt);
     }
     @Test
     void thirdMaxIntNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        int max = GenericMaximumNumber.maximum(13, 16, 11);
-        assertEquals(11,max);
+        int maxInt = intObj.maximum(13, 16, 11);
+        assertEquals(11,maxInt);
     }
     @Test
     void firstMaxFloatFNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        Float max = GenericMaximumNumber.maximum(1.3f, 1.6f, 1.1f);
-        assertEquals(1.1f,max);
+        Float maxInt = floatObj.maximum(1.3f, 1.6f, 1.1f);
+        assertEquals(1.6f,maxInt);
     }
     @Test
     void secondMaxFloatNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        Float max = GenericMaximumNumber.maximum(1.3f, 1.6f, 1.1f);
-        assertEquals(11,max);
+        Float maxFloat = floatObj.maximum(1.3f, 1.6f, 1.1f);
+        assertEquals(1.3f,maxFloat);
+    }
+    @Test
+    void thirdMaxFloatNumber() {
+        Float maxFloat = floatObj.maximum(11.3f, 21.6f, 13.1f);
+        assertEquals(21.6f, maxFloat);
     }
     @Test
     void firstMaxStringNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        String max = GenericMaximumNumber.maximum("apple", "pear", "orange");
-        assertEquals(11,max);
+        String maxString = stringObj.maximum("apple", "pear", "orange");
+        assertEquals("pear",maxString);
     }
     @Test
     void secondMaxStringNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        String max = GenericMaximumNumber.maximum("ram", "rohan", "sita");
-        assertEquals(11,max);
+        String maxString = stringObj.maximum("ram", "rohan", "sita");
+        assertEquals("sita",maxString);
     }
     @Test
     void thirdMaxStringNumber(){
-        GenericMaximumNumber tc = new GenericMaximumNumber();
-        String max = GenericMaximumNumber.maximum("bird", "animal", "human");
-        assertEquals(11,max);
+        String maxString = stringObj.maximum("bird", "animal", "human");
+        assertEquals("animal",maxString);
     }
 
 }
